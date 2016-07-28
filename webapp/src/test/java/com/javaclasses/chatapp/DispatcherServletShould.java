6c8b075a981c6f38ed20b928b2af5d17398cdadf
Kateryna Senchenko <kateryna.senchenko@gmail.com>
2016-07-28 10:49:56 +0300

    Added test for dispatcher servlet

---
+++ webapp/src/test/java/com/javaclasses/chatapp/DispatcherServletShould.java
@@ -0,0 +1,64 @@
+package com.javaclasses.chatapp;
+
+
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.message.BasicNameValuePair;
+import org.junit.Test;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.apache.http.HttpHeaders.USER_AGENT;
+import static org.junit.Assert.assertEquals;
+
+
+public class DispatcherServletShould {
+
+    private final HttpClient client = HttpClientBuilder.create().build();
+
+    @Test
+    public void acceptRegistrationRequest() throws IOException {
+
+        final String url = "http://localhost:8080/registration";
+
+        final String username = "Balto";
+        final String password = "thecallofthewild";
+
+        List<NameValuePair> parameters = new ArrayList<>();
+        parameters.add(new BasicNameValuePair("username", username));
+        parameters.add(new BasicNameValuePair("password", password));
+        parameters.add(new BasicNameValuePair("confirmPassword", password));
+
+        HttpPost postRequest = new HttpPost(url);
+        postRequest.setHeader("User-Agent", USER_AGENT);
+
+        postRequest.setEntity(new UrlEncodedFormEntity(parameters));
+
+        HttpResponse postResponse = client.execute(postRequest);
+
+        BufferedReader reader = new BufferedReader(
+                new InputStreamReader(postResponse.getEntity().getContent()));
+
+        StringBuilder result = new StringBuilder();
+
+        String line;
+        while ((line = reader.readLine()) != null) {
+            result.append(line);
+        }
+
+        String expectedStatus = "HTTP/1.1 200";
+        String expectedResult = "Balto";
+
+        assertEquals("Unexpected response status", expectedStatus, postResponse.getStatusLine().toString());
+        assertEquals("Post request failed", expectedResult, result.toString());
+
+    }
+}
